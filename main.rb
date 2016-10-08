b17766ea80ebe9b725dd47d1281a4f4b8709479f
57160613 <56160315@go.buu.ac.th>
2016-10-08 15:44:36 +0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,5 +1,7 @@
-#7
-
+#5
+US_states = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
+puts "ข้อ 5 "
+US_states.each{|i| puts i if i[-1]=="a"&&i[0]=="M"||i[-1]=="a"&&i[0]=="N"||i[-1]=="a"&&i[0]=="S"||i[-1]=="e"&&i[0]=="M"||i[-1]=="e"&&i[0]=="N"||i[-1]=="e"&&i[0]=="S"||i[-1]=="i"&&i[0]=="M"||i[-1]=="i"&&i[0]=="N"||i[-1]=="i"&&i[0]=="S"||i[-1]=="o"&&i[0]=="M"||i[-1]=="o"&&i[0]=="N"||i[-1]=="o"&&i[0]=="S"||i[-1]=="u"&&i[0]=="M"||i[-1]=="u"&&i[0]=="N"||i[-1]=="u"&&i[0]=="S"}
 
 #8
 US_states.each { |key,value| puts value if value[-1] == value[0]}  
